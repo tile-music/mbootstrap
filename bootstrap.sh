36344b71a06dee31046c1bd7a49e5b2e432f01b6
samranda <samrandaofficial@gmail.com>
2025-12-18 16:02:46 -0500

    Fixed cron + systemd installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+# Store the directory where this script is located
+BOOTSTRAP_DIR="$(cd "$(dirname "$0")" && pwd)"
+
 # install deps
 sudo apt-get update
 sudo apt-get install -y docker.io docker-compose-v2 git ufw
@@ -18,8 +21,8 @@ docker compose build
 docker compose run --rm musicbrainz createdb.sh -fetch
 
 # install cron + systemd
-sudo cp ../cron/musicbrainz-indexes /etc/cron.d/
-sudo cp ../systemd/musicbrainz.service /etc/systemd/system/
+sudo cp "$BOOTSTRAP_DIR/cron/musicbrainz-indexes" /etc/cron.d/
+sudo cp "$BOOTSTRAP_DIR/systemd/musicbrainz.service" /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable musicbrainz.service
 
